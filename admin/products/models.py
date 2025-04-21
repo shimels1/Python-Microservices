82c83d5b6e5b27713d9767909ae3fd8d0d719783
shimels1 <shimelsalem@gmail.com>
2025-04-21 01:37:41 -0900

    Add Django API to manage products

--- admin/products/models.py
+++ admin/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    link = models.PositiveIntegerField(max_length=200)
+
+class User(models.Model):
+    pass
